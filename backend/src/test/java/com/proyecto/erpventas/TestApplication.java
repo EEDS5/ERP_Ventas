379eaaee682c39f8d67f7dcec4aa5f47c1a765a9
Elias Duran <duransandoval777@gmail.com>
2025-05-27 16:51:25 -0400

    Eliminar TestApplication para evitar conflicto de SpringBootConfiguration

--- backend/src/test/java/com/proyecto/erpventas/TestApplication.java
+++
@@ -1,8 +0,0 @@
-// src/test/java/com/proyecto/erpventas/TestApplication.java
-package com.proyecto.erpventas;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class TestApplication {
-}
